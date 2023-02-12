0c8e27e28d3a90023a41be207d43482cd258c4e5
Paweł J <jpawz@outlook.com>
2023-02-12 17:51:54 +0100

    Add getWinner function

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const gameBoard = (() => {
     ["", "", ""],
   ];
 
+  const xWin = "xxx";
+  const oWin = "ooo";
+
   let mark = "o";
 
   const getBoard = () => {
@@ -30,9 +33,63 @@ const gameBoard = (() => {
     }
   };
 
+  const getWinner = () => {
+    // check rows
+    for (const row of board) {
+      if (row.join("") == xWin) {
+        return "x";
+      }
+      if (row.join("") == oWin) {
+        return "o";
+      }
+    }
+
+    // check columns
+    for (let i = 0; i < board.length; i++) {
+      let result = "";
+      for (let j = 0; j < board.length; j++) {
+        result += board[j][i];
+      }
+      if (result == xWin) {
+        return "x";
+      }
+      if (result == oWin) {
+        return "o";
+      }
+    }
+
+    // check diagonal 1
+    let diagonalResult = "";
+    for (let i = 0; i < board.length; i++) {
+      diagonalResult += board[i][i];
+    }
+    if (diagonalResult == xWin) {
+      return "x";
+    }
+    if (diagonalResult == oWin) {
+      return "o";
+    }
+
+    // check diagonal 2
+    diagonalResult = "";
+    for (let i = board.length - 1; i >= 0; i--) {
+      let j = board.length - 1 - i;
+      diagonalResult += board[i][j];
+    }
+    if (diagonalResult == xWin) {
+      return "x";
+    }
+    if (diagonalResult == oWin) {
+      return "o";
+    }
+
+    return "";
+  };
+
   return {
     getBoard,
     placeMark,
+    getWinner,
   };
 })();
 
@@ -56,6 +113,12 @@ const displayController = (() => {
       cell.addEventListener("click", () => {
         gameBoard.placeMark(rowIndex, colIndex);
         updateGrid();
+        if(gameBoard.getWinner() == "x") {
+          alert("X - win!");
+        }
+        if(gameBoard.getWinner() == "o") {
+          alert("O - win!");
+        }
       })
     )
   );
